6457ba0e4d100115af65014ec1cd9a9bf1d7e1ec
marraen <bacapetr95@gmail.com>
2016-02-24 09:47:06 +0100

    Update Cviko_3.cpp
    updated

--- Cviko_3.cpp
+++ Cviko_3.cpp
@@ -1,23 +1,24 @@
-
 // cviko_3.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
 #include <iostream>
-using namespace std;
-
 #include "trida.h"
+using namespace std;
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
 	Complex *C = new Complex;
 
 	C->set_real(5);
-	C->set_imag(3);
-	cout << C->modul() << endl;
-
+	C->set_imag(5);
+	C->modul();
+	cout << "faze je" << C->faze() << endl;
 	delete C;
+	
+	system("pause");
     return 0;
 }
 
