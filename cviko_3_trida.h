f5e60b0045a5cbe077dc22fef3aa35d7bb47016c
marraen <bacapetr95@gmail.com>
2016-02-24 09:46:46 +0100

    Update cviko_3_trida.h
    updated

--- cviko_3_trida.h
+++ cviko_3_trida.h
@@ -1,11 +1,22 @@
+#pragma once
+#include "math.h"
 
 class Complex
 {
+	double real, imag;
 
 public:
 	void set_real(double r);
 	void set_imag(double i);
 
-	double modul(void);
+	double modul(void)
+	{
+		return sqrt(real*real + imag*imag);
+	}
+
+	double faze(void)
+	{
+		return (acos(imag))/modul();
+	}
 
 };
