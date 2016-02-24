4937b4731785c3f47eb7621a85f8e379a9e70dbd
marraen <bacapetr95@gmail.com>
2016-02-24 10:07:19 +0100

    Update cviko_3_trida.cpp
    hopefully final version

--- cviko_3_trida.cpp
+++ cviko_3_trida.cpp
@@ -1,7 +1,9 @@
 #include "stdafx.h"
+#define _USE_MATH_DEFINES
 #include "math.h"
 #include <iostream>
 #include "trida.h"
+
 using namespace std;
 
 void Complex::set_real(double r)
@@ -11,4 +13,27 @@ void Complex::set_real(double r)
 void Complex::set_imag(double i)
 {
 	imag = i;
+}
+double Complex::get_real(void)
+{
+	return real;
+}
+
+double Complex::get_imag(void)
+{
+	return imag;
+}
+
+double Complex::modul(void)
+{
+	return sqrt(real*real + imag*imag);
+}
+double Complex::faze(void)
+{
+	return acos(imag / sqrt(real*real + imag*imag))*(180/M_PI);
+}
+
+void Complex::tisk(void)
+{
+	cout << "vase cislo je" << get_real() << "+j" << get_imag() << endl;
 }
