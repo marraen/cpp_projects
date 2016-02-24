dc44e06b9db2e6f9b5e9367bda7263914f84faad
marraen <bacapetr95@gmail.com>
2016-02-24 09:46:26 +0100

    Update cviko_3_trida.cpp
    update

--- cviko_3_trida.cpp
+++ cviko_3_trida.cpp
@@ -1,21 +1,8 @@
 #include "stdafx.h"
 #include "math.h"
-
-
-class Complex
-{
-	double real, imag;
-
-public:
-	void set_real(double r);
-	void set_imag(double i);
-
-	double modul (void)
-	{
-		return sqrt(real*real + imag*imag);
-	}
-	
-};
+#include <iostream>
+#include "trida.h"
+using namespace std;
 
 void Complex::set_real(double r)
 {
