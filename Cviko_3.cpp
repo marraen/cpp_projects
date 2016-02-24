49c40cf19454960da39173df63209ee091d35073
marraen <bacapetr95@gmail.com>
2016-02-24 09:07:19 +0100

    main seminer 3
    will be uploaded

--- Cviko_3.cpp
+++ Cviko_3.cpp
@@ -1,1 +1,23 @@
 
+// cviko_3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+#include "trida.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	Complex *C = new Complex;
+
+	C->set_real(5);
+	C->set_imag(3);
+	cout << C->modul() << endl;
+
+	delete C;
+    return 0;
+}
+
