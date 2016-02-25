788ee50d8f12b859193071485934a9b185a92b75
marraen <bacapetr95@gmail.com>
2016-02-25 19:10:48 +0100

    Update Cviko_2.cpp

--- Cviko_2.cpp
+++ Cviko_2.cpp
@@ -1,23 +1,33 @@
+// ConsoleApplication3.cpp : Defines the entry point for the console application.
+//
 
 #include "stdafx.h"
 #include <iostream>
-using namespace std;
 
 #include <stdio.h>
+#include <vector>
 #include "operace.h"
-int main(int argc, _TCHAR* argv[])
-{
-	int o;
-	printf("vyberte z operaci:\n1) bublinkove trideni \n2) cramerovo pravidlo\n");
-	scanf_s("%d", &o);
 
-	if (o == 1) {
-		bublinkove();
+int main(int argc, char** argv)
+
+{
+	int choice;
+	printf("vyberte z operaci:\n1) bublinkove trideni \n2) cramerovo pravidlo\n");
+	scanf_s("%d", &choice);
+
+	std::vector<int> values;
+	for (int i = 1; i < argc; ++i) {
+		values.push_back(atoi(argv[i]));
 	}
-	else if (o == 2) {
+
+	if (choice == 1) {
+		bublinkove(values);
+
+	}
+/*	else if (o == 2) {
 		cramerovo();
 	}
-
+*/
 	system("pause");
 	return 0;
 }
